461df34ba84b5ac017a8b09059b24b382c3ff46a
Eduardo Santiago <eduardo.augusto.santiago@gmail.com>
2022-05-12 14:08:47 +0100

    Corrected deploy and packages

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,6 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const compiledFactory = require('./build/CampaignFactory.json');
+
 
 const provider = new HDWalletProvider(
   'REPLACE_WITH_YOUR_MNEMONIC',
@@ -16,11 +17,10 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
-  console.log(interface);
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
